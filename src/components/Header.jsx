11a2582a57d80a565cec0af666e971370cf9f5fb
Damir <damircszar@mail.ru>
2025-05-02 17:06:58 +0300

    Первый коммит

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return <header className='header'>
+
+        <img src='https://i.pinimg.com/736x/6b/a4/74/6ba4740df15f169d3d01b297221eefcd.jpg' />
+
+    </header>
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const Profile = () => {
+    return <div className='content'>
+    <div>
+      <img src='https://avatars.mds.yandex.net/i?id=21bd43bcdb03ecb286bc613c0cc06b52657e782a-4353206-images-thumbs&n=13' />
+    </div>
+    <div>
+      ava + description
+    </div>
+    <div>
+      My posts
+    </div>
+    <div>
+      New post
+    </div>
+    <div>
+      Post 1
+    </div>
+    <div>
+      Post 2
+    </div>
+
+
+  </div>
+
+
+}
+
+export default Profile;
